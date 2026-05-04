9a9822309fa744a91668a56c9fbf66568cd84068
Blake8690 <blake.joeseph08@gmail.com>
2026-05-04 14:01:13 +0200

    Add Firebase key to email script environment variables

--- send_email.py
+++ send_email.py
@@ -1,29 +1,24 @@
 name: Run Python script
-
 on:
   schedule:
     - cron: "*/5 * * * *"
   workflow_dispatch:
-
 jobs:
   run-script:
     runs-on: ubuntu-latest
     permissions:
       contents: write
-
     steps:
       - name: Checkout repository
         uses: actions/checkout@v4
-
       - name: Set up Python
         uses: actions/setup-python@v5
         with:
           python-version: "3.9"
-
       - name: Install dependencies
         run: pip install -r requirements.txt
-
       - name: Run script
         run: python send_email.py
         env:
           EMAIL_PASSWORD: ${{ secrets.EMAIL_PASSWORD }}
+          FIREBASE_KEY: ${{ secrets.FIREBASE_KEY }}
